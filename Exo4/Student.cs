26a40627dfcbb0d2ec237a41395a4198c8ded3f9
Nassim SI MOHAMMED <nassimsimohammed@MacBook-Air-de-Nassim.local>
2023-02-14 13:12:35 +0100

    Implement exo4

--- Exo4/Student.cs
+++ Exo4/Student.cs
@@ -3,13 +3,31 @@ namespace Exo4
 {
 	public class Student
 	{
-		public String Fullname = "";
-		public double AvgGrade;
-		public double NBCourse;
+		public String Fullname { get; set; } = "";
+		public double AVGGrade { get; set; }
+		public double NBCourses { get; set; }
 
 		public Student()
 		{
 		}
-	}
+
+        public Student(String Fullname, double AVGGrade, double NBCourses)
+        {
+			this.Fullname = Fullname;
+			this.AVGGrade = AVGGrade;
+			this.NBCourses = NBCourses;
+        }
+
+        delegate void AvgChanged(double average);
+
+        AvgChanged avgChangedDelegate = new AvgChanged(RecordGrade);
+
+        private static void RecordGrade(double average)
+        {
+			double AVGGrades = AVGGrade * NBCourses + ) / (NBCourses + 1);
+        }
+
+
+    }
 }
 
